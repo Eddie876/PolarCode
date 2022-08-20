12b6a7e68f50f809f50e504246d35a1076ae25d8
Eddie876 <eddietsai.tw@gmail.com>
2022-08-20 13:51:04 +0800

    add decode

--- PolarCode/PolarCode.cpp
+++ PolarCode/PolarCode.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 const int N = sizeof(ReliabilitySequence) / sizeof(int);
 
-vector<int> CalculateMatrix(vector<int> arrayLeft, vector<int> arrayRight) {
+vector<int> EncodeArray(vector<int> arrayLeft, vector<int> arrayRight) {
     vector<int> newArray(arrayLeft.size() * 2);
     for (int i = 0; i < arrayLeft.size(); i++)
     {
@@ -18,7 +18,7 @@ vector<int> CalculateMatrix(vector<int> arrayLeft, vector<int> arrayRight) {
     return newArray;
 }
 
-vector<vector<int>> CalculateLayer(vector<vector<int>> matrix) {
+vector<vector<int>> EncodeMatrix(vector<vector<int>> matrix) {
     int layer = log2(N);
     while (layer > 0) {
         layer--;
@@ -27,7 +27,33 @@ vector<vector<int>> CalculateLayer(vector<vector<int>> matrix) {
         for (int i = 0; i < newMatrix.size(); i++) {
             auto arrayLeft = matrix[2 * i];
             auto arrayRight = matrix[2 * i + 1];
-            newMatrix[i] = CalculateMatrix(arrayLeft, arrayRight);
+            newMatrix[i] = EncodeArray(arrayLeft, arrayRight);
+        }
+        matrix = newMatrix;
+    }
+    return matrix;
+}
+
+pair<vector<int>, vector<int>> DecodeArray(vector<int> array) {
+    vector<int> arrayLeft(array.begin(), array.begin() + array.size() / 2);
+    vector<int> arrayRight(array.begin() + array.size() / 2, array.end());
+    for (int i = 0; i < arrayLeft.size(); i++)
+    {
+        arrayLeft[i] = arrayLeft[i] ^= arrayRight[i];
+    }
+    return { arrayLeft , arrayRight };
+}
+
+vector<vector<int>> DecodeMatrix(vector<vector<int>> matrix) {
+    int layer = log2(N);
+    while (layer > 0) {
+        layer--;
+        int size = pow(2, log2(N) - layer);
+        vector<vector<int>> newMatrix(size);
+        for (int i = 0; i < matrix.size(); i++) {
+            auto result = DecodeArray(matrix[i]);
+            newMatrix[2 * i] = result.first;
+            newMatrix[2 * i + 1] = result.second;
         }
         matrix = newMatrix;
     }
@@ -77,20 +103,16 @@ void PrintMatrix(vector<vector<int>> matrix) {
     cout << "}";
 }
 
-int main() {
-    cout << "請輸入message數：";
-    int size;
-    cin >> size;
-
+void Calculate(int size) {
     //產生K個bit的message，並將前面的frozen bits補滿0
     auto message = CreateMessage(size);
-    cout << "message：" << endl;
+    cout << "Message：" << endl;
     PrintArray(message);
     cout << endl << endl;
 
     //將message依Reliability Sequence排序
     auto sortedMessages = SortMessage(message);
-    cout << "sorted messages：" << endl;
+    cout << "Sorted Message：" << endl;
     PrintArray(sortedMessages);
     cout << endl << endl;
 
@@ -99,15 +121,29 @@ int main() {
     for (int i = 0; i < N; i++) {
         matrix[i] = { sortedMessages[i] };
     }
-    cout << "matrix：" << endl;
+    cout << "Message Matrix：" << endl;
     PrintMatrix(matrix);
     cout << endl << endl;
 
-    //進行運算
-    auto result = CalculateLayer(matrix);
-    cout << "result：" << endl;
-    PrintMatrix(result);
+    //進行編碼
+    auto encodedMessage = EncodeMatrix(matrix);
+    cout << "Encoded Message：" << endl;
+    PrintMatrix(encodedMessage);
+    cout << endl << endl;
 
-    return 0;
+    //進行解碼
+    auto result = DecodeMatrix(encodedMessage);
+    cout << "Decoded Message：" << endl;
+    PrintMatrix(result);
+    cout << endl << endl << "============================================================" << endl << endl;
+}
+
+int main() {
+    while (true) {
+        cout << "請輸入message數：";
+        int size;
+        cin >> size;
+        Calculate(size);
+    }    
 }
 
